00ff7cd3639821aae5d0c05ef12e0f819d641941
Drew <drewcross06@gmail.com>
2021-01-27 13:11:30 -0600

    Media ow

--- lib/Coveringarray/CoveringarrayImpl.py
+++ lib/Coveringarray/CoveringarrayImpl.py
@@ -25,7 +25,7 @@ class Coveringarray:
     ######################################### noqa
     VERSION = "0.0.1"
     GIT_URL = "https://github.com/DrewCross/Coveringarray"
-    GIT_COMMIT_HASH = "64f090c56df8125a3c1fa64c03b82f61ecb83ddb"
+    GIT_COMMIT_HASH = "fc6ed79b45d5c87a16b51e6f4777e89d4566f12a"
 
     #BEGIN_CLASS_HEADER
     #END_CLASS_HEADER
@@ -109,7 +109,7 @@ class Coveringarray:
 
            # wsClient = workspaceService(config['workspace-url'],token=token)
 
-            media = self.dfu.get_objects({'object_refs': [medianame]})[1]
+            media = self.dfu.get_objects({'object_refs': [medianame]})
 
             print('\n\n ======' + str(media.items()) + '=======\n\n')
                # for modnames in params['container_object']
