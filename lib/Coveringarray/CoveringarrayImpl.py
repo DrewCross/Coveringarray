6b58092c06a134f0864a03ed1dad30510d4c3cc3
Drew <drewcross06@gmail.com>
2021-09-03 16:45:29 -0500

    Media definition ticked

--- lib/Coveringarray/CoveringarrayImpl.py
+++ lib/Coveringarray/CoveringarrayImpl.py
@@ -376,15 +376,15 @@ class Coveringarray:
             media_data_list = []
 
             for index1, case in enumerate(matrixData['data']):
-                media_compounds_data = []
+                media_compounds_data = []##BELOW ISSUE: On CDG TESTS, sizes go from 20 -> 9 on 3 compound isolations, why are ~50% of reactions changing to sub 0?
                 for index2, compound in enumerate(case): ###BELOW: Maybe? Is object creation tied to the test suite? I Dont remember...
                     if float(compound) > 0: ##Compound filtering for trimmed makeups will conflict with test suite expected outcome of Coveing Array Tool
-                        media_compound = make_compound(matrixData['column_ids'][index2],100,-100,float(compound))
+                        media_compound = make_compound(matrixData['column_ids'][index2],.001,-100,float(compound))
                         media_compounds_data.append(copy.deepcopy(media_compound))
                 media_data = {
                 'mediacompounds':copy.deepcopy(media_compounds_data),
                 'isMinimal':0,
-                'isDefined':0,
+                'isDefined':1,
                 'type':'Undefined',
                 'name':params['output_media']+str(index1),
                 'id':params['output_media']+str(index1)
